e909613af155d7b4ea680a49698ac845becea8f9
Test <test@Tests-MacBook-Pro.local>
2026-05-24 10:53:51 +0530

    chore: connected nestjs to postgresql with typeorm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DATABASE_HOST'),
+        port: Number(configService.get<string>('DATABASE_PORT', '5432')),
+        username: configService.get('DATABASE_USER'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // auto creates tables — only for development
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
